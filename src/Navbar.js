accf68d9a040a5b23846b955fd8c454bdd15f36d
Assaad Howayek <howayekassaad@gmail.com>
2022-06-29 13:10:22 -0400

    updated nav bar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,7 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import {NavLink} from "react-router-dom";
 
 const MyNavbar = () => {
     const navbarStyle={
@@ -15,14 +16,14 @@ const MyNavbar = () => {
     return (  
         <Navbar expand="lg" style={navbarStyle}>
             <Container fluid>
-                <Navbar.Brand href="/" style={textStyle}>Apex Fitness</Navbar.Brand>
+                <Navbar.Brand to="/" style={textStyle}>Apex Fitness</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link to="/" style={textStyle}>About Us</Nav.Link>
-                    <Nav.Link to="/memberships" style={textStyle}>Our Memberships</Nav.Link>
-                    <Nav.Link to="/machines" style={textStyle}>Our Machines</Nav.Link>
-                    <Nav.Link to="/reviews" style={textStyle}>Leave a Review</Nav.Link>
+                    <NavLink to="/" style={textStyle}>About Us</NavLink>
+                    <NavLink to="/memberships" style={textStyle}>Our Memberships</NavLink>
+                    <NavLink to="/machines" style={textStyle}>Our Machines</NavLink>
+                    <NavLink to="/reviews" style={textStyle}>Leave a Review</NavLink>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
