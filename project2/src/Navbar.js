05850ee640d8247bd9ef948729dc679f8e5f6c14
Assaad Howayek <howayekassaad@gmail.com>
2022-06-29 11:09:32 -0400

    Project2 Commit

---
+++ project2/src/Navbar.js
@@ -0,0 +1,33 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+const MyNavbar = () => {
+    const navbarStyle={
+        background: 'rgba(100, 100, 255, 15)',
+        position: 'sticky'
+    };
+
+    const textStyle = {
+        color: "white",
+    };
+
+    return (  
+        <Navbar expand="lg" style={navbarStyle}>
+            <Container fluid>
+                <Navbar.Brand href="/" style={textStyle}>Apex Fitness</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/" style={textStyle}>About Us</Nav.Link>
+                    <Nav.Link href="/memberships" style={textStyle}>Our Memberships</Nav.Link>
+                    <Nav.Link href="/machines" style={textStyle}>Our Machines</Nav.Link>
+                    <Nav.Link href="/reviews" style={textStyle}>Leave a Review</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default MyNavbar;
